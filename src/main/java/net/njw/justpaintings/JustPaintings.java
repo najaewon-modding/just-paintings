734fbfef25a25aed8e2c44fe7a451440ce68ca05
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-08 18:43:11 +0900

    Register custom painting content

--- src/main/java/net/njw/justpaintings/JustPaintings.java
+++ src/main/java/net/njw/justpaintings/JustPaintings.java
@@ -5,7 +5,9 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.njw.justpaintings.network.PaintingPayloads;
 import net.njw.justpaintings.network.UploadPayloads;
+import net.njw.justpaintings.registry.ModContent;
 import net.njw.justpaintings.server.PaintingUploadManager;
 
 @Mod(JustPaintings.MOD_ID)
@@ -13,7 +15,10 @@ public final class JustPaintings {
     public static final String MOD_ID = "njw_just_paintings";
 
     public JustPaintings(IEventBus modEventBus) {
+        ModContent.ITEMS.register(modEventBus);
+        ModContent.ENTITIES.register(modEventBus);
         modEventBus.addListener(UploadPayloads::register);
+        modEventBus.addListener(PaintingPayloads::register);
         NeoForge.EVENT_BUS.addListener(JustPaintings::registerCommands);
     }
 
