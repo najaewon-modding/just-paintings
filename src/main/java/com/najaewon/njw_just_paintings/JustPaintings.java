9e4162ff7c4969a3c0166fc16de3d74b8c73d34a
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-08 17:02:35 +0900

    Add minimal mod entrypoint

---
+++ src/main/java/com/najaewon/njw_just_paintings/JustPaintings.java
@@ -0,0 +1,11 @@
+package com.najaewon.njw_just_paintings;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(JustPaintings.MOD_ID)
+public final class JustPaintings {
+    public static final String MOD_ID = "njw_just_paintings";
+
+    public JustPaintings() {
+    }
+}
