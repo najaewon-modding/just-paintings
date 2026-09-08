da8a418d37f16f8b44cce5e9bdefd47d1df89807
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-08 18:58:20 +0900

    Fix CameraRenderState import for NeoForge 26.1

--- src/main/java/net/njw/justpaintings/client/CustomPaintingRenderer.java
+++ src/main/java/net/njw/justpaintings/client/CustomPaintingRenderer.java
@@ -8,7 +8,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
-import net.minecraft.client.renderer.state.CameraRenderState;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.Identifier;
 import net.njw.justpaintings.entity.CustomPaintingEntity;
